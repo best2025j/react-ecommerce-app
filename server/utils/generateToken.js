8659badf1272a99061cf885a39c21a5301f4c227
best2025j <audugodstime95@gmail.com>
2025-06-06 17:12:33 +0100

    pages added

---
+++ server/utils/generateToken.js
@@ -0,0 +1,10 @@
+// server/utils/generateToken.js
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ _id: id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+module.exports = generateToken;
